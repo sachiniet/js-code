6c9be2f5d5bb6a9690ff4d1be3ea943d6e7011a9
sachiniet <122801443+sachiniet@users.noreply.github.com>
2023-08-23 09:34:00 -0700

    added Datatype convesion.js

---
+++ Datatype conversion.js
@@ -0,0 +1,40 @@
+
+
+//1=>true,0=>false
+//""=> false,"sachin"=>true
+
+let someNumber = 45;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// convert in number
+
+//"33"=>33
+//"36abc"=>NaN
+//true=>1,false=>0
+
+let score = "undefined"
+//let scoreNumber=Number(score);
+//console.log(scoreNumber)            45
+//console.log(typeof scoreNumber)       Number
+
+let scoreNumber = Number(score);
+console.log(scoreNumber)
+console.log(typeof scoreNumber)
+
+
+
+let LoggedIn = 1;
+let IsLoggedIn = Boolean(LoggedIn)
+console.log(LoggedIn)
+console.log(IsLoggedIn)
+
+//1=>true,0=>false
+
+
+
+
+
+
